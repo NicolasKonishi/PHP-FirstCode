0127be0f1b206dbe983e45e27f2fc2089c45e2da
NicolasKonishi <145286206+NicolasKonishi@users.noreply.github.com>
2025-08-19 09:51:03 -0300

    aprendendo funções

--- screen-match.php
+++ screen-match.php
@@ -1,7 +1,9 @@
 <?php
 // echo "Bem-vindo(a) ao screen match\n";
 
-$nomeFilme = "Top Gun"; //string
+$nomeFilme = "Top Gun - Maverick"; //string
+$nomeFilme = "Thor: Ragnarok";
+$nomeFilme = "Se beber não case";
 // $anoLancamento = 2020; //int
 $anoLancamento = $argv[1] ?? 2022; //argumento para entrada de dados //?? op de coalescência nula (caso não exita x -> op=y)
 $somaDeNotas =  8 + 3 + 2 + 2 +12;
@@ -13,4 +15,22 @@ $incluidoPrime = $planoPrime || $anoLancamento < 2019;
 echo "nome do filme: ". $nomeFilme . "\n";
 echo "nota do filme:  $notaFilme\n ";
 echo "ano do filme:  $anoLancamento\n ";
-//oi
+
+// trabalhando com decisões
+if ($anoLancamento > 2024) {
+    echo("esse filme é novo!\n");
+}else if ($anoLancamento >2020 && $anoLancamento <= 2024){
+    echo"esse filme ainda é novo\n";
+}else{
+    echo "esse filme não é um lançamento\n";
+}
+
+//Código omitido
+
+$genero = match ($nomeFilme){
+    "Top Gun - Maverick" => "ação",
+    "Thor: Ragnarok" => "super-heroi",
+    "Se beber não case" => "comédia",
+    default => "genero desconhecido",
+};
+echo $genero;
